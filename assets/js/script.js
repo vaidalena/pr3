2b1e440caebd368db3ca53271ee4caa4d72498a1
Helena <lenavaida15@gmail.com>
2021-10-25 00:27:11 +0200

    changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
+var slideIndex=1;
+
 function Slide(index, title, background, link ) {
    this.title = title;
    this.background = background;
    this.link = link;
    this.id = "slide-" + index;
+}
+
+setSlide = function(id, nom){
+    let btn = document.createElement('button');
+    btn.className = 'btn'+id;
+    btn.id = 'button-'+ id;
+    btn.innerHTML = id;
+    btn.addEventListener('click',()=>{
+        while(slideIndex!=id) Slider.nextSlide(id);
+    })
+    document.getElementById('btns').appendChild(btn);
 }
 
 const Slider = {
@@ -21,7 +34,7 @@ const Slider = {
        for(let slide of this.slides) {
 //зверніть увагу на можливість використання ``,яка дозволяє додавати в string змінні ${}
            sliderHTML +=
-               `<div id='${slide.id}' class='singleSlide'
+               `<div id='${slide.id}'  class='singleSlide'
            style='background-image:url(${slide.background});'>
            <div class='slideOverlay'>
            <h2>${slide.title}</h2>
@@ -39,6 +52,9 @@ const Slider = {
            next = this.current - 1;
        }
 
+       if(slideIndex==1) slideIndex=3;
+       else slideIndex-=1;
+
        document.getElementById("slide-" + next).style.left = "-100%";
        document.getElementById("slide-" + this.current).style.left = 0;
 
@@ -54,6 +70,9 @@ const Slider = {
        } else {
            next = this.current + 1;
        }
+
+       if(slideIndex==3) slideIndex=1;
+       else slideIndex+=1;
 
        document.getElementById("slide-" + next).style.left = "100%";
        document.getElementById("slide-" + this.current).style.left = 0;
